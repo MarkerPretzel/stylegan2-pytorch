67f2efdea766c27a41b10e5824eefe34ca0decb4
MarkerPretzel <62911272+MarkerPretzel@users.noreply.github.com>
2021-12-14 04:39:55 -0500

    Update generate_vid_nidx.py

--- generate_vid_nidx.py
+++ generate_vid_nidx.py
@@ -106,7 +106,7 @@ if __name__ == "__main__":
             truncation_latent=trunc,
             input_is_latent=True,
         )   
-        for j, idx in enumerate(args.nidx):
+        for j, idx in enumerate(args.idxs):
             if j != 0:
                 direction = degree * eigvec[:, idx].unsqueeze(0)
                 img, _ = g(
