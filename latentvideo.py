51d88cd7bc206a9d1cc96f9b1a678860b5cf3791
MarkerPretzel <62911272+MarkerPretzel@users.noreply.github.com>
2021-12-14 01:12:32 -0500

    Update latentvideo.py

--- latentvideo.py
+++ latentvideo.py
@@ -5,6 +5,9 @@ from torchvision import utils
 
 from model import Generator
 
+import cv2
+import os
+
 
 if __name__ == "__main__":
     torch.set_grad_enabled(False)
@@ -64,6 +67,9 @@ if __name__ == "__main__":
     latent = torch.randn(args.n_sample, 512, device=args.device)
     latent = g.get_latent(latent)
     
+    
+    dir_path = f"{args.out_prefix}_index-{args.index}_degree-{args.degree}/"
+    
     degree = - args.degree
     degree_per_frame = (args.degree * 2) // args.n_sample
     for frame in range(args.n_sample):
@@ -76,8 +82,44 @@ if __name__ == "__main__":
         )    
         grid = utils.save_image(
         img,
-        f"{args.out_prefix}_index-{args.index}_degree-{args.degree}_test.png",
+        dir_path + f"{args.out_prefix}_index-{args.index}_degree-{degree}.png",
         normalize=True,
         range=(-1, 1),
         nrow=0,
     )
+        
+    ### The following code is copied from http://tsaith.github.io/combine-images-into-a-video-with-python-3-and-opencv-3.html ###
+    ext = 'png'
+    output = f"{args.out_prefix}_index-{args.index}_degree-{args.degree}"
+    
+    images = []
+    for f in os.listdir(dir_path):
+        if f.endswith(ext):
+            images.append(f)
+
+    # Determine the width and height from the first image
+    image_path = os.path.join(dir_path, images[0])
+    frame = cv2.imread(image_path)
+    cv2.imshow('video',frame)
+    height, width, channels = frame.shape
+
+    # Define the codec and create VideoWriter object
+    fourcc = cv2.VideoWriter_fourcc(*'mp4v') # Be sure to use lower case
+    out = cv2.VideoWriter(output, fourcc, 20.0, (width, height))
+
+    for image in images:
+
+        image_path = os.path.join(dir_path, image)
+        frame = cv2.imread(image_path)
+
+        out.write(frame) # Write out frame to video
+
+        cv2.imshow('video',frame)
+        if (cv2.waitKey(1) & 0xFF) == ord('q'): # Hit `q` to exit
+            break
+
+# Release everything if job is finished
+out.release()
+cv2.destroyAllWindows()
+
+print("The output video is {}".format(output))
